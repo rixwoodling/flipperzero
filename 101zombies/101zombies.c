b39018aa21d179bb932c08cc7d6c6e94fcf64f0f
Rix <rixwoodling@gmail.com>
2025-06-27 09:14:17 -0700

    Update 101zombies.c

--- 101zombies/101zombies.c
+++ 101zombies/101zombies.c
@@ -1,3 +1,4 @@
+  GNU nano 7.2                                                          101zombies.c                                                                    
 #include <stdio.h>
 #include <furi.h>
 #include <gui/gui.h>
@@ -5,8 +6,21 @@
 static void draw_callback(Canvas* canvas, void* ctx) {
     UNUSED(ctx);
     canvas_clear(canvas);
-    canvas_set_font(canvas, FontPrimary);
-    canvas_draw_str(canvas, 32, 40, "101 Zombies!");
+    //canvas_set_font(canvas, FontPrimary);
+    //canvas_draw_str(canvas, 32, 15, "101 Zombies!");
+
+    canvas_set_font(canvas, FontSecondary);
+    //int y = 30;
+    int y = 10;
+
+    canvas_draw_str(canvas, 1, y += 1, "Choose to FIGHT or RUN away");
+    canvas_draw_str(canvas, 1, y += 8, "from the zombie horde! Chance");
+    canvas_draw_str(canvas, 1, y += 8, "for new weapons when fighting.");
+    canvas_draw_str(canvas, 1, y += 8, "However, consecutive fighting");
+    canvas_draw_str(canvas, 1, y += 8, "increases zombie damage. Chance");
+    canvas_draw_str(canvas, 1, y += 8, "for medikits when running. How-");
+    canvas_draw_str(canvas, 1, y += 8, "ever, constant running is for");
+    canvas_draw_str(canvas, 1, y += 8, "weak little girls! Good luck.");
 }
 static void input_callback(InputEvent* input_event, void* ctx) {
     furi_assert(ctx);
