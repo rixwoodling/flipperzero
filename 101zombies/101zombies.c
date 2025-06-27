71af9aa6bbeb7684fc4e8550cb68eb1c0e12c43f
Rix <rixwoodling@gmail.com>
2025-06-27 16:05:19 -0700

    Update 101zombies.c

--- 101zombies/101zombies.c
+++ 101zombies/101zombies.c
@@ -8,11 +8,15 @@ typedef enum {
 //    StateMainGame,
 //    StateBackMenu,
 //    StateGameOver,
+    StateConfirmExit,
 } GameState;
 
 typedef struct {
     GameState screen;
+    uint8_t selection;
 } AppState;
+
+
 
 static void draw_callback(Canvas* canvas, void* ctx) {
     furi_assert(ctx);
@@ -37,6 +41,18 @@ static void draw_callback(Canvas* canvas, void* ctx) {
             canvas_draw_str(canvas, 1, 40, "However, constant running is");
             canvas_draw_str(canvas, 1, 50, "for weak little girls!");
             canvas_draw_str(canvas, 1, 60, "Good luck.");
+            break;
+        case StateConfirmExit:
+            canvas_set_font(canvas, FontSecondary);
+            canvas_draw_str(canvas, 10, 10, "Give up?");
+
+            if(state->selection == 0) {
+                canvas_draw_str(canvas, 10, 30, "> No");
+                canvas_draw_str(canvas, 10, 40, "  Yes, I'm scared.");
+            } else {
+                canvas_draw_str(canvas, 10, 30, "  No");
+                canvas_draw_str(canvas, 10, 40, "> Yes, I'm scared.");
+            }
             break;
     }
 }
@@ -65,18 +81,37 @@ int32_t zombies_main(void* p) {
         furi_check(furi_message_queue_get(event_queue, &event, FuriWaitForever) == FuriStatusOk);
 
         if(event.key == InputKeyBack) {
-            break;
-        } else if(event.key == InputKeyOk) {
+            app_state.screen == StateConfirmExit; //
+            app_state.selection = 0;              //
+            view_port_update(view_port);          //
+            continue;                             //
+        }
+
+        if(event.key == InputKeyOk) {
             switch(app_state.screen) {
-               case StateTitle:
-                   app_state.screen = StateRules;
-                   break;
-               case StateRules:
-                   app_state.screen = StateTitle;
-                   break;
+                case StateTitle:
+                    app_state.screen = StateRules;
+                    break;
+                case StateRules:
+                    app_state.screen = StateTitle;
+                    break;
+                case StateConfirmExit:
+                    if(event.key == InputKeyUp || event.key == InputKeyDown) {
+                        app_state.selection ^= 1;
+                    } else {
+                        if(app_state.selection == 0) {
+                            app_state.screen = StateTitle;
+                        } else {
+                            break;
+                        }
+                    }
+                    view_port_update(view_port);
+                    break;
+                default:
+                    break;
             }
             view_port_update(view_port);
-       }
+        }
     }
 
     furi_message_queue_free(event_queue);
